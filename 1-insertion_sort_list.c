ef56c83399a16caf4e4abc68be69540e22cc807e
adamdebalke <youngadam65@gmail.com>
2022-01-12 09:54:00 +0300

    Swap two integers in an array

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 
 /**
- * swap_nodes - Swap two nodes in a listint_t doubly-linked list.
+ * swap_nodes - Swap two nodes in a listint_t doubly-linked list
  * @h: A pointer to the head of the doubly-linked list.
  * @n1: A pointer to the first node to swap.
  * @n2: The second node to swap.
--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,15 +1,17 @@
 #include "sort.h"
 
-void swap_ints(int *a, int *b);
-int lomuto_partition(int *array, size_t size, int left, int right);
-void lomuto_sort(int *array, size_t size, int left, int right);
-void quick_sort(int *array, size_t size);
-
 /**
  * swap_ints - Swap two integers in an array
  * @a: The first integer to swap.
  * @b: The second integer to swap.
  */
+
+void swap_ints(int *a, int *b);
+int lomuto_partition(int *array, size_t size, int left, int right);
+void lomuto_sort(int *array, size_t size, int left, int right);
+void quick_sort(int *array, size_t size);
+
+
 void swap_ints(int *a, int *b)
 {
 	int tmp;
